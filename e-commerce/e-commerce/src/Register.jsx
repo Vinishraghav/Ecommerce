74280d4762960287bf8146bf3f3e9b4181e64ef2
Vinishraghav <vinish523@gmail.com>
2025-12-19 11:50:05 +0530

    Update registration API endpoint URL

--- e-commerce/e-commerce/src/Register.jsx
+++ e-commerce/e-commerce/src/Register.jsx
@@ -15,7 +15,9 @@ export default function Register() {
       return;
     }
 
-    const res = await fetch("http://localhost:5000/api/auth/register", {
+    const res = await fetch("
+http://51.20.121.208:5000
+/api/auth/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(form),
