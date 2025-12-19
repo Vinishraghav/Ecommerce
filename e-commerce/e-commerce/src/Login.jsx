17d572e51f7ac1c72987ee4c93887bb256eb407e
Vinishraghav <vinish523@gmail.com>
2025-12-19 11:48:56 +0530

    Update Login.jsx

--- e-commerce/e-commerce/src/Login.jsx
+++ e-commerce/e-commerce/src/Login.jsx
@@ -18,7 +18,7 @@ export default function Login({ setUser, loadCart }) {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("http://16.170.227.170:3000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
